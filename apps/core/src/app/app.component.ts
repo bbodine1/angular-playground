874794897aecfb54add854d42dd1b23a07984f36
Brad Bodine <bbodine1@gmail.com>
2024-08-07 19:09:05 -0500

    add ui lib

--- apps/core/src/app/app.component.ts
+++ apps/core/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiComponent } from '@angular-playground/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, UiComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
