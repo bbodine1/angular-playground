c16f7702407ba076f0a79c4519a0261bead793b7
Brad <bbodine1@gmail.com>
2024-08-21 21:05:06 -0500

    update footer component layout and styles

--- core-ui/src/lib/footer/footer.component.ts
+++ core-ui/src/lib/footer/footer.component.ts
@@ -1,22 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   template: `
     <footer>
       <div class="language">
         <div class="inner">
-          <div>Language</div>
-          <a>English</a>
+          <img src="lang.svg" alt="" />
+          <a class="active">English</a>
           <a>Español</a>
           <a>Amharic</a>
         </div>
       </div>
 
-      <div class="container">container</div>
+      <div class="container">
+        <div class="inner">
+          <div class="brand">
+            <b>CoverME.gov</b>
+            <p>All Rights Reserved.</p>
+          </div>
+
+          <div class="help">
+            <b>Get Help</b>
+            <button>Live Chat</button>
+            <button>(866) 636-0355 / TTY 711</button>
+          </div>
+        </div>
+      </div>
     </footer>
   `,
   styles: `
@@ -34,7 +46,7 @@ import { CommonModule } from '@angular/common';
     }
 
     .language {
-      background-color: var(--secondary-bg);
+      background-color: var(--secondary-bg, #F5F5F5);
       height: 36px;
 
       .inner {
@@ -42,17 +54,52 @@ import { CommonModule } from '@angular/common';
         align-items: center;
         height: 100%;
         gap: 12px;
-        max-width: var(--page-max-width);
+        max-width: var(--page-max-width, 1140px);
         margin: 0 auto;
+        color: #41454A;
+      }
 
-
-
+      a:not(.active) {
+        text-decoration: underline;
+        cursor: pointer;
+        color: #3D5D6F;
       }
     }
 
     .container {
-      background-color: var(--primary-bg);
+      display: grid;
+      background-color: var(--primary-bg, #E1DFDD);
       height: 80px;
+
+      .inner {
+        display: grid;
+        grid-template-columns: 1fr max-content;
+        align-items: center;
+        align-self: center;
+        max-width: var(--page-max-width, 1140px);
+        margin: 0 auto;
+        height: 48px;
+        width: 100%;
+      }
+
+      .brand {
+        font-size: 16px;
+        color: #323130;
+
+        p {
+          margin: 0;
+        }
+      }
+
+      .help {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        height: 100%;
+        background-color: #ffffff;
+        border-radius: 24px;
+        padding: 0 12px;
+      }
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
